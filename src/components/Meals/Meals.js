788bbf39988f147457c30fad4af4bfa8cacc1054
May <maykhine.ada@gmail.com>
2023-10-11 15:52:06 -0400

    work on meals components

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+// import classes from "./Meal.mdoule.css"
+import { MealsSummary } from "./MealsSummary"
+import { AvailableMeals } from "./AvailableMeals"
+export const Meals = () => {
+  return (
+    <div>
+      <MealsSummary></MealsSummary>
+      <AvailableMeals></AvailableMeals>
+    </div>
+  )
+}
